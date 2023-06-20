63f278f6f080890436af8eace39c70a98076c8f9
PreetishChoudhary <96870076+PreetishChoudhary@users.noreply.github.com>
2023-06-21 09:58:17 +1000

    Update main.js

--- API/Spotify/main.js
+++ API/Spotify/main.js
@@ -2,7 +2,7 @@ function Oauth(){
     const clientId = 'd9a800ac4ead4627ab1280e84dc5593c';
     const redirectUri = 'https://preetishchoudhary.github.io/API/Spotify/callback.html';
     const responseType = 'token';
-    const scopes = 'user-read-private user-read-email user-read-playback-state user-read-recently-played'; // Add the required scopes
+    const scopes = 'user-read-private user-read-email user-read-playback-state user-read-recently-played user-modify-playback-state'; // Add the required scopes
 
     const authorizationUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}`;
 
@@ -68,4 +68,26 @@ function GetPlayData(){
         .catch(error => {
           console.error('Error:', error);
         });
+}
+
+function Player(request){
+    if(request == "pause"){
+        fetch(https://api.spotify.com/v1/me/player/play, {
+            method: "PUT",
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('spotifyAccessToken'),
+                    'Content-Type': 'application/json'
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Request failed with status ' + response.status);
+                }
+                console.log('Playback paused.');
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+        }
+    }
 }
