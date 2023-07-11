61af92e2b3cff9f5189fb8026c369ba18b9719f0
PreetishChoudhary <96870076+PreetishChoudhary@users.noreply.github.com>
2023-07-11 11:19:59 +1000

    Update main.js

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,13 +1,1 @@
-const container = document.querySelector('.parallax-container');
-const desk = document.querySelector('.desk');
-const computer = document.querySelector('.computer');
-
-container.addEventListener('mousemove', function (event) {
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
-
-  // Adjust the position of the elements based on mouse movement
-  desk.style.transform = `translate(calc(-50% + ${mouseX * 0.02}px), calc(-50% + ${mouseY * 0.02}px))`;
-  computer.style.transform = `translate(calc(-50% + ${mouseX * 0.05}px), calc(-50% + ${mouseY * 0.05}px))`;
-});
 
