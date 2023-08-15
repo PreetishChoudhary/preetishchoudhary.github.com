dba6ac5f0839e1394d18c6714c417b5925434fb6
PreetishChoudhary <96870076+PreetishChoudhary@users.noreply.github.com>
2023-08-15 12:50:51 +1000

    Update main.js

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,71 +1,23 @@
-class TextScramble{
-  constuctor(rand){
-    this.rand = rand
-    this.chars = "<>?:\"{}|_+!@#$%^&*(),./;\'[]\-=`~"
-    this.update   = this.update.bind(this)
-  }
-  setText(textNew){
-    const textCurrent = this.rand.innerText
-    const length = Math.max(textCurrent.length, textNew.length)
-    const promise = new Promise((resolve) => this.resolve = resolve)
-    for(let i =0; i<length; i++){
-      const from = textCurrent[i] || ''
-      const to = textNew[i] || ''
-      const start = Math.floor(Math.random() * 40)
-      const end = start + Math.floor(Math.random() * 40)
-      this.queue.push({ from, to, start, end })
-    }
-    cancelAnimationFrame(this.frameRequest)
-    this.frame = 0
-    this.update()
-    return promise
-  }
-  update() {
-    let output = ''
-    let complete = 0
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
-      if (this.frame >= end) {
-        complete++
-        output += to
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar()
-          this.queue[i].char = char
-        }
-        output += `<span class="dud">${char}</span>`
-      } else {
-        output += from
-      }
-    }
-    this.rand.innerHTML = output
-    if (complete === this.queue.length) {
-      this.resolve()
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update)
-      this.frame++
-    }
-  }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)]
-  }
+function goHome(){
+  document.getElementsByClassName("Home").style.display = ;
+  document.getElementsByClassName("About").style.display = 'none';
+  document.getElementsByClassName("Work").style.display = 'none';
+  document.getElementsByClassName("Lab").style.display = 'none';
+  document.getElementsByClassName("Contact").style.display = 'none';
 }
 
-const Phrases = [
-  "Welcome",
-  "To my Portfolio",
-  "Preetish Choudhary"
-]
-
-const rand = document.querySelector('#head')
-const fx = new TextScramble(rand)
-
-let counter = 0
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800)
-  })
-  counter = (counter + 1) % phrases.length
+function goAbout(){
+  
 }
 
-next()
+function goWork(){
+  
+}
+
+function goLab(){
+  
+}
+
+function goContact(){
+  
+}
